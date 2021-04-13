401215b37afdb43045ba06ec6d0b8cfb5f4d7f04
nemo9955 <nemo9955@gmail.com>
2021-04-13 18:35:07 +0300

    add recursivity

--- src/modules/GuiManager.ts
+++ src/modules/GuiManager.ts
@@ -47,6 +47,8 @@ import * as d3 from "d3"
 type guiElem = {
     tag: string,
     attr?: {},
+    style?: {},
+    html?: {}[] | string,
     listeners?: string,
     // listeners?: {} | {}[] | string,
 }
@@ -64,33 +66,55 @@ var testData: guiJson = [
             type: "button",
             id: "12312",
             value: "button AAA",
-            class: "btn btn-primary btn-sm",
+            class: "btn btn-primary btn-sm ",
         }
     },
     {
         tag: "input",
         listeners: "click",
         attr: {
+            class: "btn btn-primary btn-sm",
             type: "button",
             id: "346546",
             value: "button BBB",
-            class: "btn btn-primary btn-sm",
         }
     },
     // {
-    //     tag: "br",
-    // },
+    //     tag: "div",
+    //     attr: {
+    //         // class: "col",
+    //         class: "container",
+    //         id: "5sd4f5s4d",
+    //     },
+    //     html: [
+    {
+        tag: "div",
+        html: "click",
+        // style: {
+        //     width: "100px",
+        // },
+        attr: {
+            // class: "w-40",
+            class: "text-start container",
+        }
+    },
     {
         tag: "input", // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
         listeners: "input",
         // listeners: "change",
+        // style: {
+        //     width: "100px",
+        // },
         attr: {
+            // class: "w-40",
             type: "color",
             id: "g3v56h56y",
             value: "#ff0000",
             name: "color picker",
         }
     },
+    //     ],
+    // },
     // {
     //     tag: "br",
     // },
@@ -115,8 +139,30 @@ export class GuiManager {
 
     constructor() {
         this.container = d3.select("body")
+            // .append("div")
+            // .attr("class", "border")
             .append("div")
-            .attr("class", "container")
+            // .attr("class", "container")
+            // .attr("class", "container bg-light border shadow-sm rounded ")
+            .attr("class", "d-grid gap-1 bg-light border shadow-sm rounded ")
+            // .attr("class", "d-grid gap-1 col-6 mx-auto")
+            // .attr("class", "row")
+            .style("zIndex", "200")
+            .style("position", "fixed")
+            .style("top", "10px")
+            .style("left", "10px")
+            .style("width", "256px")
+
+
+        // var selectElem: HTMLElement = this.slectPane.containerElem_
+        // selectElem.className = "SelectionDiv"
+        // selectElem.style.zIndex = "200"
+        // selectElem.style.position = "fixed"
+        // selectElem.style.bottom = "0px"
+        // selectElem.style.right = "0px"
+        // selectElem.style.width = "256px"
+
+
 
         setTimeout(() => { this.test1(); }, 1000);
     }
@@ -125,52 +171,6 @@ export class GuiManager {
         return `${d?.tag}-${d?.attr?.id}`
     }
 
-    test1() {
-        var dat = this.container.selectAll("*")
-            .data(testData, this.getId)
-        // .join(
-        //     enter => enter.call(this.addTag.bind(this)),
-        //     update => { update.each(this.setTag.bind(this)); return update; },
-        //     remove => remove.call(this.removeTag.bind(this))
-        // )
-
-        dat.enter().call(this.enterSelectTags.bind(this))
-        dat.exit().call(this.exitSelectTags.bind(this))
-
-        setTimeout(() => { this.test2(); }, 1000);
-    }
-
-    test2() {
-        testData.push({
-            tag: "input",
-            listeners: "click",
-            attr: {
-                type: "button",
-                id: "afgasdfg",
-                value: "button DDD",
-                class: "btn btn-primary btn-sm",
-            }
-        })
-
-        var dat = this.container.selectAll("*")
-            .data(testData, this.getId)
-
-        dat.enter().call(this.enterSelectTags.bind(this))
-        dat.exit().call(this.exitSelectTags.bind(this))
-
-        setTimeout(() => { this.test3(); }, 1000);
-    }
-
-    test3() {
-        testData.shift();
-        console.log("testData", testData);
-
-        var dat = this.container.selectAll("*")
-            .data(testData, this.getId)
-
-        dat.enter().call(this.enterSelectTags.bind(this))
-        dat.exit().call(this.exitSelectTags.bind(this))
-    }
 
     private addTag(datum: guiElem, index: number, groups: any[]) {
         var domElem = groups[index]
@@ -180,6 +180,9 @@ export class GuiManager {
                 console.log("event", event);
                 // console.log("datEv", datEv);
             })
+
+
+
     }
 
     private setTag(datum: guiElem, index: number, groups: any[]) {
@@ -190,6 +193,24 @@ export class GuiManager {
             for (const key in datum.attr) {
                 d3Elem.attr(key, datum.attr[key])
             }
+
+        if (datum?.style)
+            for (const key in datum.style) {
+                d3Elem.style(key, datum.style[key])
+            }
+
+        if (typeof datum?.html == "string")
+            d3Elem.html(datum.html)
+        else if (Array.isArray(datum?.html)) {
+            console.log("datum", datum);
+            var dat = d3Elem
+                .selectChildren("*")
+                .data(datum.html, this.getId)
+            dat.enter().call(this.enterSelectTags.bind(this))
+            dat.exit().call(this.exitSelectTags.bind(this))
+        }
+        // else if (typeof datum?.html == "object") {
+        // }
     }
 
     private removeTag(datum: guiElem, index: number, groups: any[]) {
@@ -217,8 +238,67 @@ export class GuiManager {
     }
 
     private exitSelectTags(remove: d3UpdateType) {
+        remove.each(console.log)
         remove.remove();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    test1() {
+        var dat = this.container.selectChildren("*")
+            .data(testData, this.getId)
+        // .join(
+        //     enter => enter.call(this.addTag.bind(this)),
+        //     update => { update.each(this.setTag.bind(this)); return update; },
+        //     remove => remove.call(this.removeTag.bind(this))
+        // )
+        dat.enter().call(this.enterSelectTags.bind(this))
+        dat.exit().call(this.exitSelectTags.bind(this))
+
+        setTimeout(() => { this.test2(); }, 1000);
+    }
+
+    test2() {
+        testData.push({
+            tag: "input",
+            listeners: "click",
+            attr: {
+                type: "button",
+                id: "afgasdfg",
+                value: "button DDD",
+                class: "btn btn-primary btn-sm",
+            }
+        })
+
+        var dat = this.container.selectChildren("*")
+            .data(testData, this.getId)
+        dat.enter().call(this.enterSelectTags.bind(this))
+        dat.exit().call(this.exitSelectTags.bind(this))
+
+        setTimeout(() => { this.test3(); }, 1000);
+    }
+
+    test3() {
+        testData.shift();
+        console.log("testData", testData);
+
+        var dat = this.container.selectChildren("*")
+            .data(testData, this.getId)
+        dat.enter().call(this.enterSelectTags.bind(this))
+        dat.exit().call(this.exitSelectTags.bind(this))
+    }
 
 }
